9fdfb67f60e73dcb70083fc6a2ce9f85ce265a5b
Ilia Baranov <ibaranov@clearpathrobotics.com>
2015-07-02 15:23:21 -0400

    added controls for arm

--- src/ridgeback_demo.py
+++ src/ridgeback_demo.py
@@ -1,18 +1,27 @@
 #! /usr/bin/env python
-import roslib;
-import rospy
+import roslib; roslib.load_manifest('ur_driver')
 import time
+import rospy
+import actionlib
+from control_msgs.msg import *
+from trajectory_msgs.msg import *
 import math
-
 import sys
 import numpy as np
-
-import actionlib
 import tf
 from robotiq_s_model_control.msg import *
 import geometry_msgs.msg
 from geometry_msgs.msg import Pose
 from sensor_msgs.msg import Joy
+
+JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
+               'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+
+Q1 = [1.57,-1.57,0,-1.57,0,0]
+Q2 = [-1.57,-1.57,0,-1.57,0,0]
+Q3 = [-1.57,0,-2.79,-1.57,0,0]
+
+client = None
 
 msg = SModel_robot_output()
 msg.rACT =1	
@@ -46,10 +55,13 @@ def joy_call(input):
         msg.rPRA = 0
 
 if __name__ == '__main__':
+    global client
     try:
-
-        arm_pub = rospy.Publisher('/husky_ur5Arm', Pose, queue_size = 1)
-        msgArm = Pose()
+        rospy.init_node("test_move", anonymous=True, disable_signals=True)
+        client = actionlib.SimpleActionClient('arm/follow_joint_trajectory', FollowJointTrajectoryAction)
+        print "Waiting for arm server..."
+        client.wait_for_server()
+        print "Connected to arm server"
 
         finger_pub = rospy.Publisher('/SModelRobotOutput', SModel_robot_output, queue_size=1)  
 
@@ -61,6 +73,19 @@ if __name__ == '__main__':
         while not rospy.is_shutdown():         
             finger_pub.publish(msg)
             rate.sleep()
+            g = FollowJointTrajectoryGoal()
+            g.trajectory = JointTrajectory()
+            g.trajectory.joint_names = JOINT_NAMES
+            g.trajectory.points = [
+                JointTrajectoryPoint(positions=Q1, velocities=[0]*6, time_from_start=rospy.Duration(2.0)),
+                JointTrajectoryPoint(positions=Q2, velocities=[0]*6, time_from_start=rospy.Duration(3.0)),
+                JointTrajectoryPoint(positions=Q3, velocities=[0]*6, time_from_start=rospy.Duration(6.0))]
+            client.send_goal(g)
+            try:
+                client.wait_for_result()
+            except KeyboardInterrupt:
+                client.cancel_goal()
+                raise
 
         print('Done!')
 
